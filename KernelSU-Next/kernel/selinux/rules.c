86d60a72df3894ce7e38b8f80953fb2c8a9ba119
bcrtvkcs <kc.sipahi@gmail.com>
2026-02-27 15:59:08 +0300

    selinux: fix pointer assignment in rules.c for kernel 5.4

--- KernelSU-Next/kernel/selinux/rules.c
+++ KernelSU-Next/kernel/selinux/rules.c
@@ -17,7 +17,7 @@ static struct policydb *get_policydb(void)
 {
 	struct policydb *db;
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 7, 0)
-	db = selinux_state.ss->policydb;
+	db = &selinux_state.ss->policydb;
 #else
 	struct selinux_policy *policy = selinux_state.policy;
 	db = &policy->policydb;
