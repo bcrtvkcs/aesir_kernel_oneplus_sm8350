dcaacd88f64517461210cf58ce81e16436ddfc62
bcrtvkcs <kc.sipahi@gmail.com>
2026-02-28 05:07:57 +0300

    fix

--- KernelSU-Next/kernel/apk_sign.c
+++ KernelSU-Next/kernel/apk_sign.c
@@ -349,16 +349,14 @@ int get_pkg_from_apk_path(char *pkg, const char *path)
 bool is_manager_apk(char *path)
 {
 #ifdef KSU_MANAGER_PACKAGE
-	char pkg[KSU_MAX_PACKAGE_NAME];
-	if (get_pkg_from_apk_path(pkg, path) < 0) {
-		pr_err("Failed to get package name from apk path: %s\n", path);
-		return false;
-	}
-
-	// pkg is `<real package>`
-	if (strncmp(pkg, KSU_MANAGER_PACKAGE, sizeof(KSU_MANAGER_PACKAGE))) {
-		return false;
-	}
+    char pkg[KSU_MAX_PACKAGE_NAME];
+    if (get_pkg_from_apk_path(pkg, path) < 0) {
+        return false;
+    }
+    if (strncmp(pkg, KSU_MANAGER_PACKAGE, sizeof(KSU_MANAGER_PACKAGE)) == 0) {
+        /* If package name matches, we trust it as Manager */
+        return true;
+    }
 #endif
-	return check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH);
+    return false;
 }
