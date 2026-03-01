05dc79a07c70c2e7733c75e8c9992d73e571e860
bcrtvkcs <kc.sipahi@gmail.com>
2026-03-01 22:31:08 +0300

    Fix

--- KernelSU-Next/kernel/setuid_hook.c
+++ KernelSU-Next/kernel/setuid_hook.c
@@ -14,7 +14,9 @@
 #ifdef CONFIG_KSU_SUSFS
 #include <linux/susfs_def.h>
 #endif // #ifdef CONFIG_KSU_SUSFS
-
+#ifndef TWA_RESUME
+#define TWA_RESUME true
+#endif
 #include "allowlist.h"
 #include "setuid_hook.h"
 #include "klog.h" // IWYU pragma: keep
