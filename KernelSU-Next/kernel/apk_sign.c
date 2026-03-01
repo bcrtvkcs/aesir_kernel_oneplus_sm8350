971b334c45ead17ba5f91b40a6490cc3e5737b1a
bcrtvkcs <kc.sipahi@gmail.com>
2026-03-01 08:24:55 +0300

    Fix

--- KernelSU-Next/kernel/apk_sign.c
+++ KernelSU-Next/kernel/apk_sign.c
@@ -354,11 +354,12 @@ bool is_manager_apk(char *path)
 		pr_err("Failed to get package name from apk path: %s\n", path);
 		return false;
 	}
-
 	// pkg is `<real package>`
 	if (strncmp(pkg, KSU_MANAGER_PACKAGE, sizeof(KSU_MANAGER_PACKAGE))) {
 		return false;
 	}
-#endif
+	return true;
+#else
 	return check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH);
+#endif
 }
