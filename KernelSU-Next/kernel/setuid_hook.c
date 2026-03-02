4950305e5fbcd156191f3931de5cbbab4e958e3e
bcrtvkcs <kc.sipahi@gmail.com>
2026-03-02 21:08:38 +0300

    Fix

--- KernelSU-Next/kernel/setuid_hook.c
+++ KernelSU-Next/kernel/setuid_hook.c
@@ -124,9 +124,10 @@ int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid){
     //   will always return true, that's why we need to explicitly check if new_uid belongs to
     //   ksu manager
     if (ksu_get_manager_appid() == new_uid % PER_USER_RANGE) {
-
-        pr_info("install fd for manager: %d\n", new_uid);
         ksu_manager_spawn_pid = current->pid;
+        
+        pr_info("install fd for manager: %d\n", new_uid);
+        
         struct callback_head *cb = kzalloc(sizeof(*cb), GFP_ATOMIC);
         if (!cb)
             return 0;
