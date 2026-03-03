afbbb41043c09938383b5b22be3d7bbf6f0ae4da
K. Cihan Sipahi <32391714+bcrtvkcs@users.noreply.github.com>
2026-03-03 16:31:19 +0300

    Update ram.c

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -781,6 +781,7 @@ static int ramoops_parse_dt(struct platform_device *pdev,
 
 static int ramoops_probe(struct platform_device *pdev)
 {
+	pr_err("ramoops: probe called\n");
 	struct device *dev = &pdev->dev;
 	struct ramoops_platform_data *pdata = dev->platform_data;
 	struct ramoops_platform_data pdata_local;
