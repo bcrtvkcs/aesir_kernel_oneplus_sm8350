1da890ffc14d071d64a045306155bdb893387dcc
bcrtvkcs <kc.sipahi@gmail.com>
2026-03-11 22:46:58 +0300

    susfs: Fix mnt_id comparison: use == instead of >= for DEFAULT_KSU_MNT_ID

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -109,7 +109,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
-			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+			r->mnt_id == DEFAULT_KSU_MNT_ID &&
 			!susfs_is_current_ksu_domain())
 	{
 		return 0;
@@ -154,7 +154,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
-			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+			r->mnt_id == DEFAULT_KSU_MNT_ID &&
 			!susfs_is_current_ksu_domain())
 	{
 		return 0;
@@ -226,7 +226,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
-			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+			r->mnt_id == DEFAULT_KSU_MNT_ID &&
 			!susfs_is_current_ksu_domain())
 	{
 		return 0;
