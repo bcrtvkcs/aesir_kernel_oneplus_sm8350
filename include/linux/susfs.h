650a9855556d779fc10c3d6a3537d3f30cbe6272
bcrtvkcs <kc.sipahi@gmail.com>
2026-03-14 10:51:08 +0300

    susfs: fix st_susfs_sus_path struct layout to match userspace binary
    ksu_susfs binary expects st_susfs_sus_path to contain:
      unsigned long  target_ino
      char           target_pathname[SUSFS_MAX_LEN_PATHNAME]
      unsigned int   i_uid
      int            err
    
    Kernel struct was missing target_ino and i_uid fields, causing
    copy_from_user to read target_pathname from wrong offset.
    Kernel was reading inode number bytes as pathname, resulting in
    incorrect path registration (e.g. inode 52 = ASCII '4').
    
    These fields are not used by susfs_add_sus_path() or
    susfs_add_sus_path_loop() kernel functions, they exist solely
    to maintain ABI compatibility with the userspace binary.

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -27,7 +27,9 @@
 /* sus_path */
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 struct st_susfs_sus_path {
+	unsigned long                           target_ino;
 	char                                    target_pathname[SUSFS_MAX_LEN_PATHNAME];
+	unsigned int                            i_uid;
 	int                                     err;
 };
 
